a848771ffe05b68f9de232ce845b15c8e8f4b661
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-25 17:20:08 -0300

    chore: refactor server entry point to use path mapping

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,5 @@
-import { app } from './app.js';
-import { env } from '../../env/index.js';
+import { app } from '@/shared/infra/http/app.js';
+import { env } from '@/shared/env/index.js';
 
 const start = async (): Promise<void> => {
   try {
