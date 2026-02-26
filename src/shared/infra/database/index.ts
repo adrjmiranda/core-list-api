68856f8b2e9219fa85c3f594b6cba1c34a9a89c0
Adriano Miranda <adrjmiranda@gmail.com>
2026-02-26 11:36:26 -0300

    chore: integrate validated env variables with drizzle database conn

--- src/shared/infra/database/index.ts
+++ src/shared/infra/database/index.ts
@@ -1,12 +1,14 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 
+import { env } from '@/shared/env/index.js';
+
 import * as addresses from './drizzle/addresses.js';
 import * as contacts from './drizzle/contacts.js';
 import * as users from './drizzle/users.js';
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: env.DATABASE_URL,
 });
 
 export const db = drizzle(pool, {
