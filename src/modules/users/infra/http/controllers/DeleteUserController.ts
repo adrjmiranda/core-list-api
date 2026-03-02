ee12e0b85740c9d189e1c7d03bfff8710a40dec3
Adriano Miranda <adrjmiranda@gmail.com>
2026-03-02 15:27:57 -0300

    feat: add endpoint and service to delete user profile

---
+++ src/modules/users/infra/http/controllers/DeleteUserController.ts
@@ -0,0 +1,14 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { DeleteUserService } from '@/modules/users/services/DeleteUserService.js';
+
+export class DeleteUserController {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const userId = request.user.sub;
+
+    const deleteUserService = new DeleteUserService();
+    await deleteUserService.execute(userId);
+
+    return reply.status(204).send();
+  }
+}
---
+++ src/modules/users/services/DeleteUserService.ts
@@ -0,0 +1,10 @@
+import { eq } from 'drizzle-orm';
+
+import { users } from '@/shared/infra/database/drizzle/users.js';
+import { db } from '@/shared/infra/database/index.js';
+
+export class DeleteUserService {
+  async execute(userId: string): Promise<void> {
+    await db.delete(users).where(eq(users.id, userId));
+  }
+}
